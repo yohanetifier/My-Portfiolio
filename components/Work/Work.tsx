a3509903680a362c7814828ba414ac399ff9b016
yohan <yohan@adelios.fr>
2023-10-20 00:26:36 +0200

    advance in the transition

--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react';
 import { useThree, extend, useFrame } from '@react-three/fiber';
 import { shaderMaterial, useTexture } from '@react-three/drei';
 import * as THREE from 'three';
+import { useSpring, useMotionValue, animate } from 'framer-motion';
+import { motion } from 'framer-motion-3d';
 
 interface Props { }
 
@@ -10,48 +12,39 @@ const Work = ( props: Props ) => {
 
     const ref = useRef<THREE.ShaderMaterial>( null );
     const { gl, size, camera } = useThree();
-    // const distanceToGeometry = camera.position.z;
-    const canvasWidth = size.width;
-    const canvasHeight = size.height;
-    const cameraAspect = camera.aspect;
-    const cameraFov = camera.fov;
-
-    // useEffect( () => {
-    //     ref.current.needUpdate = true;
-    //     gl.domElement.addEventListener( 'click', ( e ) => {
-    //         console.log( e );
-    //         mouseX = e.clientX / window.innerWidth;
-    //         mouseY = 1 - ( e.clientY / window.innerHeight );
-    //     } );
-    // }, [ mouseX, mouseY, ref.current ] );
-
-    // useFrame( () => {
-    //     ref.current.uniforms.uMouse.x = mouseX;
-    //     ref.current.uniforms.uMouse.y = mouseY;
-    // } );
-
-    // gl.domElement.addEventListener( 'mousemove', ( e ) => {
-    //     mouseX = e.clientX / window.innerWidth;
-    //     mouseY = 1 - ( e.clientY / window.innerHeight );
-    // } );
-
     const urlTexture = './the-buyer.png';
     const theBuyerImage = useTexture( urlTexture );
+    const urlTexture2 = './tolefi.png';
+    const tolefiTexture = useTexture( '/tolefi.png' );
+    let uTime = 0.0;
+    let uFadeIn = 0.0;
+
+    useFrame( ( { clock } ) => {
+        ref.current.uniforms.uTime.value = clock.getElapsedTime();
+        // ref.current.uniforms.uFadeIn.value;
+    } );
 
     const uniforms =
     {
-        uTexture: theBuyerImage
+        uTexture: theBuyerImage,
+        uTolefiTexture: tolefiTexture,
+        uTime: uTime,
+        uFadeIn: uFadeIn,
     };
 
     const fragmentShader =
         `
         varying vec2 vUv;
+        varying vec3 vPosition;
         uniform sampler2D uTexture;
-       
+        uniform float uTime;
+        uniform sampler2D uTolefiTexture;
+        uniform float uFadeIn;
 
         void main () {
-            vec4 color = texture2D(uTexture, vUv);
-            gl_FragColor = vec4(color.xyz, 1.0);
+            vec4 tolefiTexture = texture(uTolefiTexture, vUv);
+            vec4 theBuyerTexture = texture(uTexture, vUv);
+            gl_FragColor = vec4(mix(theBuyerTexture.xyz * 3. ,tolefiTexture.xyz , abs(sin(uTime))), 1.0);
         }
         
         `;
@@ -59,10 +52,13 @@ const Work = ( props: Props ) => {
     const vertexShader =
         `
         varying vec2 vUv;
+        uniform float uTime; 
+        varying vec3 vPosition;
 
         void main () {
             vUv = uv;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            vPosition = position;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z , 1.);
         }
         `;
 
@@ -73,15 +69,21 @@ const Work = ( props: Props ) => {
     );
     extend( { DistortionShaderMaterial } );
 
-    const distanceToPlane = Math.abs( camera.position.z );
-    const cameraDegToRadians = camera.fov * ( Math.PI / 360 );
     // const cameraRadians = THREE.MathUtils.degToRad( camera.fov );
     // console.log( cameraDegToRadians, cameraRadians );
-    const halfWidth = distanceToPlane * Math.tan( camera.aspect * cameraDegToRadians );
-    const halfHeight = distanceToPlane * Math.tan( cameraDegToRadians );
+
+    // const distanceToPlane = Math.abs( camera.position.z );
+    // const cameraDegToRadians = camera.fov * ( Math.PI / 360 );
+    // const halfWidth = distanceToPlane * Math.tan( camera.aspect * cameraDegToRadians );
+    // const halfHeight = distanceToPlane * Math.tan( cameraDegToRadians );
+
+    const distanceToPlane = camera.position.z;
+    const degToRad = camera.fov * Math.PI / 180;
+    const fovY = distanceToPlane * Math.tan( degToRad / 2 ) * 2;
+
     return (
         <mesh >
-            <planeGeometry args={ [ halfWidth * 1.5, halfHeight * 2, 30, 30 ] } />
+            <planeGeometry args={ [ fovY * camera.aspect, fovY, 10, 10 ] } />
             <distortionShaderMaterial ref={ ref } />
         </mesh>
     );
